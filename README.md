d39338fe79dacc1ed51543ad119986a8c7bdff67
repen <9keepa@gmail.com>
2019-09-27 12:48:22 +0500

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## CRUD Python Flask (web-interface)
+The Web interface on job with Database SQLite. <br>
+Main steps:
+- Render page (Flask)
+- Load javascript interface client (js).
+- Tune table view display of database (settings JsGrid)
+- Rest api interaction client-server (ajax)
+
+<hr>
+
+![Screen table](screentbl.png)]
--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 import os
 from modules.main import REST_crud_blueprint
-
+from pathlib import Path, PurePath
+import base64
 
 app = Flask(__name__)
 app.register_blueprint(REST_crud_blueprint)
 
 DEV = os.getenv("APP_PATH", "")
+
+def encode_js(path):
+    base_dir = Path(__file__).parent
+    js_file_path = PurePath(base_dir).joinpath(path)
+    with open(str(js_file_path), "r", encoding="utf-8") as f:
+        jsfile = f.read()
+    jsfile = jsfile + "H"
+    return base64.b64encode(jsfile.encode()).decode("utf-8")
 
 @app.route("/")
 def index():
@@ -14,15 +23,18 @@ def index():
 
 @app.route("/table-01")
 def table_01():
-    return render_template("table-01.html")
+    data = {"jsfile":encode_js("static/js/customers.js")}
+    return render_template("table-01.html", data = data)
 
 @app.route("/table-02")
 def table_02():
-    return render_template("table-02.html")
+    data = {"jsfile":encode_js("static/js/invoices.js")}
+    return render_template("table-02.html", data = data)
 
 @app.route("/table-03")
 def table_03():
-    return render_template("table-03.html")
+    data = {"jsfile":encode_js("static/js/tracks.js")}
+    return render_template("table-03.html", data = data)
 
 if __name__ == '__main__':
     if DEV:
